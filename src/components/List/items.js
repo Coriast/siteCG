ea5647eb717bca6a4303df25db0695cfea966d6a
Pedro Willian <pedrowillianpesquisador@gmail.com>
2024-04-06 00:29:27 -0300

    segunda alteração de links

--- src/components/List/items.js
+++ src/components/List/items.js
@@ -3,7 +3,7 @@ import transform_matrice from "../../assets/images/transform_matrice.png";
 export const items = [
   {
     id: 1,
-    link: "/Exercises/Transformations",
+    link: "/siteCG/Exercises/Transformations",
     imgFile: transform_matrice,
     title: "Transformações",
     text: "Para o melhor entendimento de como a matriz manipula o elemento."
